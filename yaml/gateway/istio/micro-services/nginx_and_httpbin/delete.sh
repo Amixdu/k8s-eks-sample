d9e7f42b5857a8aa448ecaf7bfcd31b7a5467714
AminduKaushal <amixxdu@gmail.com>
2024-02-10 21:19:55 +0530

    fix: fixed wrong command in shell script

--- yaml/gateway/istio/micro-services/nginx_and_httpbin/delete.sh
+++ yaml/gateway/istio/micro-services/nginx_and_httpbin/delete.sh
@@ -1,5 +1,5 @@
 kubectl delete -f gateway/istio/micro-services/nginx_and_httpbin/httproute.yaml
 kubectl delete -f gateway/istio/micro-services/nginx_and_httpbin/gateway.yaml
 kubectl delete -f gateway/istio/micro-services/nginx_and_httpbin/namespace.yaml
-kubectl apply -f gateway/istio/micro-services/nginx_and_httpbin/httpbin.yaml
+kubectl delete -f gateway/istio/micro-services/nginx_and_httpbin/httpbin.yaml
 kubectl delete -f gateway/istio/micro-services/nginx_and_httpbin/nginx.yaml
